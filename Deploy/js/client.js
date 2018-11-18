e9dd9a6d7f3c8f11b9c81a9cbe3bbc6e083b55ce
Ollivander <ollivander64@gmail.com>
2018-11-18 01:34:14 +0000

    its working

--- Deploy/js/client.js
+++ Deploy/js/client.js
@@ -1,17 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function () { // displays all current events when page fully loaded due to .read()
     load();
 });
 
-function load() {
-    $.ajax({
-        url: 'https://image.maps.api.here.com/mia/1.6/mapview',
-        type: 'GET',
-        data: {
-            app_id: 'QiFKebd8bcnRbZGEcwld',
-            app_code: 'ze8W4nLeImVYnd5skk6xJw'
-        },
-        success: function (data) {
-            alert(JSON.stringify(data));
-        }
+
+function load() { // load events that meet the criteria
+    $.get("https://image.maps.api.here.com/mia/1.6/mapview?app_id=QiFKebd8bcnRbZGEcwld&app_code=ze8W4nLeImVYnd5skk6xJw", function (data) {
+
+        $('.div_imagetranscrits').html('<img src="data:image/png;base64,' + data + '" />');
     });
 }
